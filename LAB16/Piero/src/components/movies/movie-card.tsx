4b507c083243d7dce3670524b415aa338246a410
Piero-HO <piero.huaytalla@tecsup.edu.pe>
2026-07-01 17:07:27 -0500

    primer commit

---
+++ LAB16/Piero/src/components/movies/movie-card.tsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+
+import type { Movie } from "@/types/movie";
+
+import {
+  Badge,
+} from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface Props {
+  movie: Movie;
+}
+
+const MovieCard = ({ movie }: Props) => {
+  return (
+    <article>
+      <Card className="overflow-hidden">
+        <img
+          src={movie.posterUrl}
+          alt={movie.title}
+          className="aspect-2/3 w-full object-cover"
+        />
+
+        <CardHeader className="gap-3">
+          <Badge
+            variant="secondary"
+            className="w-fit"
+          >
+            {movie.genre}
+          </Badge>
+
+          <CardTitle>
+            {movie.title}
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <p className="mb-4 text-sm text-muted-foreground">
+            {movie.synopsis}
+          </p>
+
+          <Link
+            to={`/movies/${movie.id}`}
+            className="
+              text-sm
+              font-medium
+              text-blue-600
+              hover:underline
+            "
+          >
+            View details
+          </Link>
+        </CardContent>
+      </Card>
+    </article>
+  );
+};
+
+export default MovieCard;
